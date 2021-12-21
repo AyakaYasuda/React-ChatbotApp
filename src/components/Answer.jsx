83ca7c16c2b6f2e37ee737e4c3c981dbf7981937
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-12-21 11:23:34 -0800

    customized material-UI styles

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+// import { makeStyles, createStyles } from '@material-ui/styles';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
-const Answer = (props) => {
+const Answer = props => {
   return (
-    <Stack direction='row' spacing={2}>
-      <Button variant='contained' onClick={() => props.select(props.content, props.nextId)}>
-          {props.content}
-      </Button>
-    </Stack>
+    <Button
+      sx={{
+        width: '100%',
+        borderColor: '#FFB549',
+        color: '#FFB549',
+        textTransform: 'none',
+        fontSize: '12px',
+        fontWeight: 600,
+        marginBottom: '8px',
+        '&:hover': {
+          borderColor: '#FFB549',
+          backgroundColor: '#FFB549',
+          color: '#fff',
+        },
+      }}
+      variant='outlined'
+      onClick={() => props.select(props.content, props.nextId)}
+    >
+      {props.content}
+    </Button>
   );
 };
 
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import List from '@mui/material/List';
 import { Chat } from './index';
 
-const Chats = (props) => {
+const Chats = props => {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List
+      sx={{
+        width: '100%',
+        height: 400,
+        padding: 0,
+        overflow: 'auto',
+        bgcolor: 'background.paper',
+      }}
+    >
       {props.chats.map((chat, index) => {
-          return <Chat text={chat.text} type={chat.type} key={index.toString()} />
+        return (
+          <Chat text={chat.text} type={chat.type} key={index.toString()} />
+        );
       })}
     </List>
   );
